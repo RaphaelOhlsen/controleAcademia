39b47a5623621cf037e98e6f99d4021ff738df55
Raphael Ohlsen <raphael.ohlsen@gmail.com>
2020-02-17 09:21:35 -0300

    Preenchendo campos do formulário com Nunjucks

--- instructors.js
+++ instructors.js
@@ -11,16 +11,16 @@ exports.show = (req,res) => {
   });
   
   if (!foundInstructor) return res.send("instructor not found!");
-
+  
   const instructor = {
     ...foundInstructor,
     age: age(foundInstructor.birth),
     created_at: timeFormat(foundInstructor.created_at)
   }
-
+  
   foundInstructor.gender === "M" 
-    ? instructor.gender = "Masculino" : instructor.gender = "Feminino";
-
+  ? instructor.gender = "Masculino" : instructor.gender = "Feminino";
+  
   instructor.services = strToArr(foundInstructor.services);
 
   return res.render("instructors/show", { instructor });
@@ -36,13 +36,13 @@ exports.post = (req, res) => {
       return res.send("Please, fill all fields");
     }
   });
-
+  
   let {avatar_url, birth, name, services, gender} = req.body
-
+  
   birth = Date.parse(birth);
   const created_at = Date.now();
   const id = Number(data.instructors.length + 1);
-
+  
 
   data.instructors.push({
     id,
@@ -53,10 +53,22 @@ exports.post = (req, res) => {
     services,
     created_at
   });
-
+  
   fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
     if(err) return res.send("Write file error");
-    return res.redirect("/instructors")
+    return res.redirect("/instructors");
   });
- }
+}
  
+//edit
+exports.edit = (req,res) => {
+  const { id } = req.params;
+  
+  const foundInstructor = data.instructors.find(instructor => {
+    return instructor.id === Number(id)
+  });
+  
+  if (!foundInstructor) return res.send("instructor not found!");
+  
+  return res.render('instructors/edit', {instructor: foundInstructor});
+}

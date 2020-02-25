ec26d90a16206bef23e51ace6931e4ef13da601b
Raphael Ohlsen <raphael.ohlsen@gmail.com>
2020-02-25 19:01:32 -0300

    Lógica do menu ativo

--- public/scripts.js
+++ public/scripts.js
@@ -1,18 +1,7 @@
-const modalOverlay = document.querySelector('.modal-overlay');
-const cards = document.querySelectorAll('.card');
-const closeModal = document.querySelector('.close-modal');
-const iframe = modalOverlay.querySelector('iframe');
+const currentPage = location.pathname;
+const menuItens = document.querySelectorAll("header .links a");
 
-cards.forEach(card =>
-  card.addEventListener('click', ()=>{
-    const videoId = card.getAttribute('id');
-    modalOverlay.classList.add('active');
-    iframe.src = `https://www.youtube.com/embed/${videoId}`;
-  })
-)
-
-closeModal.addEventListener('click', ()=> {
-  modalOverlay.classList.remove('active');
-  iframe.src = ''
+menuItens.forEach(item => {
+    if(currentPage.includes(item.getAttribute('href')))
+      item.classList.toggle('active');
 });
-

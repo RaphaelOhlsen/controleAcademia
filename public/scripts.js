5224214a9f87e8df2f9315614c3ae12f9a05002d
Raphael Ohlsen <raphael.ohlsen@gmail.com>
2020-02-26 10:24:51 -0300

    Confirmação e exclusão de membros

--- public/scripts.js
+++ public/scripts.js
@@ -1,7 +1,13 @@
 const currentPage = location.pathname;
 const menuItens = document.querySelectorAll("header .links a");
+const formDelete = document.querySelector("#form-delete");
 
 menuItens.forEach(item => {
     if(currentPage.includes(item.getAttribute('href')))
       item.classList.toggle('active');
+});
+
+formDelete.addEventListener("submit", e => {
+  const confirmation = confirm('Deseja Deletar? ');
+  if(!confirmation) e.preventDefault();
 });

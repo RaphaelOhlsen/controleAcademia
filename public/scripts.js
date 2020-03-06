2e63dc1b04aad992cdcc81150d404e015f1ecbdc
Raphael Ohlsen <raphael.ohlsen@gmail.com>
2020-03-06 10:11:15 -0300

    Ultima modificação modulo-04

--- public/scripts.js
+++ public/scripts.js
@@ -8,6 +8,10 @@ menuItens.forEach(item => {
 });
 
 formDelete.addEventListener("submit", e => {
-  const confirmation = confirm('Deseja Deletar? ');
+  const confirmation = confirm('Deseja Deletar ? ');
   if(!confirmation) e.preventDefault();
 });
+
+
+
+teste

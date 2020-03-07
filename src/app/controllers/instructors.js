aa87ced030e38938b9fe3a26a3be49b633c1765d
Raphael Ohlsen <raphael.ohlsen@gmail.com>
2020-03-07 09:27:12 -0300

    Inserido dados no banco de dados

--- src/app/controllers/instructors.js
+++ src/app/controllers/instructors.js
@@ -1,7 +1,9 @@
 const { age, strToArr, timeFormat, date } = require('../../lib/utils');
+const db = require('../../config/db');
 
 module.exports = {
   index(req, res) {
+    // console.log('teste');
     return res.render('instructors/index');
   },
   create(req, res) {
@@ -16,10 +18,34 @@ module.exports = {
       }
     });
   
-  let {avatar_url, birth, name, services, gender} = req.body
+    const query = `
+      INSERT INTO instructors (
+        name,
+        avatar_url,
+        gender,
+        services,
+        birth,
+        created_at
+      ) VALUES ($1, $2, $3, $4, $5, $6)
+      RETURNING id
+    `;
   
-  return
+    const values = [
+      req.body.name,
+      req.body.avatar_url,
+      req.body.gender,
+      req.body.services,
+      date(req.body.birth).iso,
+      date(Date.now()).iso
+    ];
+
+    db.query(query, values, function(err, results) {
+      if(err) return res.send("Database Error!");
+
+      return res.redirect(`/instructors/${results.rows[0].id}`);
+    })
   },
+
   show(req, res) {
     return
   },

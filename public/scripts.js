f6af573f6a80de55a67d5d72fcb269f554ceee84
Raphael Ohlsen <raphael.ohlsen@gmail.com>
2020-03-17 19:34:53 -0300

    Algoritmo de paginação no front

--- public/scripts.js
+++ public/scripts.js
@@ -1,17 +1,37 @@
-const currentPage = location.pathname;
-const menuItens = document.querySelectorAll("header .links a");
-const formDelete = document.querySelector("#form-delete");
+// const currentPage = location.pathname;
+// const menuItens = document.querySelectorAll("header .links a");
+// const formDelete = document.querySelector("#form-delete");
 
-menuItens.forEach(item => {
-    if(currentPage.includes(item.getAttribute('href')))
-      item.classList.toggle('active');
-});
+// menuItens.forEach(item => {
+//     if(currentPage.includes(item.getAttribute('href')))
+//       item.classList.toggle('active');
+// });
 
-formDelete.addEventListener("submit", e => {
-  const confirmation = confirm('Deseja Deletar ? ');
-  if(!confirmation) e.preventDefault();
-});
+// formDelete.addEventListener("submit", e => {
+//   const confirmation = confirm('Deseja Deletar ? ');
+//   if(!confirmation) e.preventDefault();
+// });
 
 
+//Paginação
 
-teste
+let totalPages = 20,
+    selectedPage = 6,
+    pages = [],
+    oldPage;
+
+for(let currentPage = 1; currentPage <= totalPages; currentPage++) {
+  const fisrtAndLastPage = currentPage == 1 || currentPage == totalPages;
+  const pagesAfterSelectdPage = currentPage <= selectedPage + 2;
+  const pagesBeforeSelectecPage = currentPage >=selectedPage - 2;
+  
+  if(fisrtAndLastPage || pagesBeforeSelectecPage && pagesAfterSelectdPage) {
+    if(oldPage && currentPage - oldPage > 2) pages.push('...');
+    pages.push(currentPage);
+    oldPage = currentPage;
+  }
+  if(currentPage == totalPages - 1 && currentPage - totalPages > 2) 
+    pages.push('...');
+}
+
+console.log(pages)
